2d30f8e8bd4e3ce5ef7deec89c63a0856284448b
zzl-hyun <kimgihyun877@gmail.com>
2025-05-06 18:55:36 +0900

    Chore: Configure Prisma

--- src/db.config.js
+++ src/db.config.js
@@ -1,7 +1,10 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
+
+export const prisma = new PrismaClient();
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST, // mysql의 hostname
